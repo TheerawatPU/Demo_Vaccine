12e0154bec09e6426281cd08e3a405f28ad3ac2a
theerawat0258 <theerawat0258@gmail.com>
2026-02-20 00:51:22 +0700

    fix router for github pages deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import DatePicker from "react-datepicker";
-import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/Demo_Vaccine">
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
