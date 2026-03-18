865b3f01004b8f8c1bf645ca1a1a67e262ee5550
theerawat0258 <theerawat0258@gmail.com>
2026-03-19 03:23:37 +0700

    update bug

--- supabase/functions/line-webhook/index.ts
+++ supabase/functions/line-webhook/index.ts
@@ -1,9 +1,9 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.3'
 
-const LINE_ACCESS_TOKEN = Deno.env.get("LINE_ACCESS_TOKEN") ?? "";;
-const SUPABASE_URL = Deno.env.get("VITE_SUPABASE_URL") ?? "";;
-const SUPABASE_SERVICE_KEY = Deno.env.get("VITE_SUPABASE_ANON_KEY") ?? "";; 
+const SUPABASE_URL = Deno.env.get("SUPABASE_URL") ?? "";
+const SUPABASE_SERVICE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? ""; 
+const LINE_ACCESS_TOKEN = Deno.env.get("LINE_ACCESS_TOKEN") ?? "";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
 
